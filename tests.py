682e7ec141ee9e62fe8028a2f71ecf3b009fc149
Steve Katz <katz.stephenj@gmail.com>
2014-07-13 13:29:46 -0400

    tests!!!!!!!!!!!

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+
+import dispatch
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def test_get_distances(self):
+        f = open('test_data.json')
+        json_resp = json.loads(''.join(f.readlines()))
+        distances = dispatch.get_distances(json_resp)
+        self.assertEquals(distances, [48, 181])
+
+    def test_choose_index(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
